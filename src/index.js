26901186de4a63011ffdc5aab768eb82de8d49b2
Chipo Mazhani <chipomazhani@gmail.com>
2024-09-29 18:36:43 +0200

    api code is working

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-
-    cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(temperature);
-}
-
-function searchCity(city) {
-    let apiKey = "3t336d20d7o34c5bc08a60f4f3f8ff69";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
-    axios.get(apiUrl).then(refreshWeather);
-}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  
+
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function searchCity(city) {
+  //let city = "Paris";
+  let apiKey = "3t336d20d7o34c5bc08a60f4f3f8ff69";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
-searchCity("Paris");
+function refreshWeather(response) {
+  
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+searchCity("Tokyo");
